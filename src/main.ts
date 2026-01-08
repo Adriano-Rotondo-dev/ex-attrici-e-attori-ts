d564a1246ef7106075d547be9b9c4e1cd1f56c05
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2026-01-08 17:19:54 +0100

    feat: Milestone 1 + Milestone 2

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,34 @@
 
+// Movies tuple
+type Movies = [string, string, string];
+//Nationality 
+type Nationality =
+   "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese";
 
+
+
+type Person ={
+  readonly id: number,
+  readonly name: string,
+  birth_year: number
+  death_year?: number,
+  biography: string, 
+  image: string //Image url
+}
+
+type Actress = Person &{
+  most_famous_movies: Movies, 
+  awards: string, 
+  nationality: string
+
+}
